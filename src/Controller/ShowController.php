d99d0aa7d99d819aef863fa08cc9421d40a9ee96
willy Kouagnia <willkoua@gnail.com>
2018-06-12 22:53:49 -0400

    clean API view

--- src/Controller/ShowController.php
+++ src/Controller/ShowController.php
@@ -6,26 +6,12 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\Show;
 
-/**
- * @Route("/show")
- */
+
 class ShowController extends Controller
 {
-    /**
-     * @Route("/{id}", name="show_detail")
-     */
+
     public function detail($id)
     {
-        $show = $this->getDoctrine()->getRepository(Show::class)->find($id);
-
-        if (!$show) {
-            throw $this->createNotFoundException(
-                'No show found'
-            );
-        }
-
-        return $this->render('show/detail.html.twig', [
-            'show' => $show,
-        ]);
+        return null;
     }
 }
